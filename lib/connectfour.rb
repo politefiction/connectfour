38c13b0b578ad8f1a225d11f2154d3a683b5d28b
Chelsi Jones <cajones.bz@gmail.com>
2017-08-26 12:06:20 -0500

    Finished.

--- lib/connectfour.rb
+++ lib/connectfour.rb
@@ -6,3 +6,199 @@ class Slot
 		@token ||= token
 	end
 end
+
+class CFBoard
+	attr_accessor :slots, :victory
+
+	def initialize
+		@slots = []
+		create_slots
+		@victory = false
+	end
+
+	def create_slots
+		row = (0..5).to_a
+		col = (0..6).to_a
+		row.each { |x| col.each { |y| @slots << Slot.new([x, y]) } }
+	end
+
+	def display_board
+		puts; puts (1..7).to_a.join("   "); puts
+		cr = 0
+		while cr < 42
+			printed_row = []
+			@slots[cr..(cr+6)].each { |slot| slot.token.nil? ? printed_row << "⚊" : printed_row << slot.token }
+			puts printed_row.join("   ")
+			cr += 7
+		end
+	end
+
+	def check_board
+		check_rows
+		check_columns
+		check_diagonals
+	end
+
+	def check_rows
+		cr = 5
+		while cr >= 0
+			row = @slots.select { |slot| slot.coord[0] == cr }
+			any_matches?(row)
+			cr -= 1
+		end
+	end
+
+	def check_columns
+		cc = 0
+		while cc <= 6
+			col = @slots.select { |slot| slot.coord[1] == cc }
+			any_matches?(col)
+			cc += 1
+		end
+	end
+
+	def check_diagonals
+		@slots.each_with_index do |slot, index|
+			diag1 = [slot]
+			diag2 = [slot]
+			main_ind = index
+			@slots.each_with_index { |slot, index| diag1 << slot if index > main_ind and (index - main_ind) % 8 == 0 }
+			@slots.each_with_index { |slot, index| diag2 << slot if index > main_ind and (index - main_ind) % 6 == 0 }
+			any_matches?(diag1)
+			any_matches?(diag2)
+		end
+	end
+
+	def any_matches?(array)
+		matches = []
+		array.each do |slot|
+			unless matches.length == 4
+				if matches.include? slot.token
+					matches << slot.token
+				elsif !slot.token.nil?
+					matches = [slot.token]
+				else
+					matches = []
+				end
+			end
+		end
+		@victory = true if matches.length == 4
+	end
+
+end
+
+class CFPlayer
+	attr_accessor :id, :token, :board
+
+	@@player_count = 0
+	@@board = CFBoard.new
+
+	def initialize(token=nil)
+		@@player_count += 1
+		@id = "Player #{@@player_count}"
+		@token = token
+		@board = @@board
+	end
+
+	# For testing purposes
+	def copy_board
+		@board = @@board
+	end
+
+	def place_token(col)
+		empty_slots = @@board.slots.select { |slot| slot.coord[1] == (col.to_i - 1) and slot.token.nil? }
+		if empty_slots.empty?
+			puts "Sorry, this column is full. Please try again."
+			turn
+		else
+			empty_slots[-1].token = @token
+		end
+	end
+
+	def turn
+		puts; puts "#{self.id}'s turn. Choose a column (1-7):"
+		@@board.display_board
+		col = gets.chomp.strip
+		if col.to_i < 8 and col.to_i > 0
+			place_token(col)
+			copy_board
+		else
+			puts "Invalid entry. Please try again."
+			turn
+		end
+	end
+
+	def connect_four?
+		@@board.check_board
+		if @@board.victory
+			@@board.display_board
+			puts "Game over! #{self.id} wins!"
+		end
+	end
+
+	def self.establish_players (p1, p2)
+		[p1, p2].each do |p|
+			if p.token.nil?
+				puts "#{p.id}, choose your token (one character please):"
+				token = gets.chomp
+				p.token = token[0]
+				puts "#{p.id} has chosen #{p1.token}."
+			end
+		end
+	end
+
+	def self.game(p1=nil, p2=nil)
+		p1 ||= CFPlayer.new
+		p2 ||= CFPlayer.new
+		puts "Welcome to Connect Four!"
+		CFPlayer.establish_players(p1, p2) 
+		turns = 42
+		current_turn = p1
+		while turns > 0
+			if current_turn == p1
+				p1.turn
+				p1.connect_four?
+				CFPlayer.play_again?(p1, p2) if @@board.victory
+				current_turn = p2
+			else
+				p2.turn
+				p2.connect_four?
+				CFPlayer.play_again?(p1, p2) if @@board.victory
+				current_turn = p1
+			end
+			turns -= 1
+		end
+		@@board.display_board 
+		puts "Game over! Nobody wins!"
+		CFPlayer.play_again?(p1, p2)
+	end
+
+	def self.play_again?(p1, p2)
+		puts "Would you like to play again? (Y/N)"
+		answer1 = gets.chomp; puts
+		if answer1[0].downcase == "y"
+			puts "Would you like to change your tokens? (Y/N)"
+			answer2 = gets.chomp; puts
+			if answer2[0].downcase == "y"
+				[p1, p2].each { |p| p.token = nil } if answer2[0].downcase == "y"
+			elsif answer2[0].downcase != "n"
+				puts "Game will assume you meant 'no'."; puts
+			end
+			@@board.slots.each { |slot| slot.token = nil }
+			CFPlayer.game(p1, p2)
+		elsif answer1[0].downcase == "n"
+			puts "Thanks for playing!"
+			exit
+		else
+			puts "Invalid entry. Please try again."
+			CFPlayer.play_again?(p1, p2)
+		end
+	end
+end
+
+
+#CFPlayer.game
+
+#player1 = CFPlayer.new("⚙")
+#player2 = CFPlayer.new("♥")
+#CFPlayer.game(player1, player2)
--- spec/connectfour_spec.rb
+++ spec/connectfour_spec.rb
@@ -14,3 +14,246 @@ describe Slot do
 		end
 	end
 end
+
+describe CFBoard do
+	let(:test_board) { CFBoard.new }
+	after(:each) do
+		test_board.slots.each { |slot| slot.token = nil }
+	end
+	context 'upon creation' do
+		it 'has 42 slots' do
+			expect(test_board.slots.length).to eq(42)
+		end
+
+		it 'has 7 slots in each row' do
+			row1 = test_board.slots.select { |slot| slot.coord[0] == 0 }
+			expect(row1.length).to eq(7)
+		end
+
+		it 'has 6 slots in each column' do
+			col1 = test_board.slots.select { |slot| slot.coord[1] == 0 }
+			expect(col1.length).to eq(6)
+		end
+	end
+
+	describe '#check_rows' do
+		context 'when the row contains a Connect Four' do
+			context 'at the beginning of the row' do
+				before do
+					test_board.slots[35..38].each { |slot| slot.token = "♥" }
+					test_board.slots[39..41].each { |slot| slot.token = "♣" }
+				end
+				it 'triggers a victory' do
+					test_board.check_rows
+					expect(test_board.victory).to be true
+				end
+			end
+
+			context 'towards the end of the row' do
+				before do
+					test_board.slots[35..37].each { |slot| slot.token = "♥" }
+					test_board.slots[38..41].each { |slot| slot.token = "♣" }
+				end
+				it 'triggers a victory' do
+					test_board.check_rows
+					expect(test_board.victory).to be true
+				end
+			end
+		end
+
+		context 'when row is filled in without a Connect Four' do
+			before do
+				test_board.slots[35].token = "♥" 
+				test_board.slots[37].token = "♥" 
+				test_board.slots[36].token = "♣"
+				test_board.slots[38].token = "♣"
+			end
+			it 'does not trigger a victory' do
+				test_board.check_rows
+				expect(test_board.victory).to be false
+			end
+		end
+
+		context 'when tokens of consecutive slots in different rows match' do
+			before do
+				test_board.slots[33..36].each { |slot| slot.token = "♥" }
+			end
+			it 'does not trigger a victory' do
+				test_board.check_rows
+				expect(test_board.victory).to be false
+			end
+		end
+	end
+
+	describe '#check_columns' do
+		context 'when the column contains a Connect Four' do
+			context 'at the top of the column' do
+				before do
+					col = test_board.slots.select { |slot| slot.coord[1] == 3 }
+					col[0..3].each { |slot| slot.token = "♣" }
+					col[4..5].each { |slot| slot.token = "♥" }
+					test_board.check_columns
+				end
+				it 'triggers a victory' do
+					expect(test_board.victory).to be true
+				end
+			end
+
+			context 'at the bottom of the column' do
+				before do
+					col = test_board.slots.select { |slot| slot.coord[1] == 3 }
+					col[0..1].each { |slot| slot.token = "♣" }
+					col[2..5].each { |slot| slot.token = "♥" }
+					test_board.check_columns
+				end
+				it 'triggers a victory' do
+					expect(test_board.victory).to be true
+				end
+			end
+		end
+
+		context 'when column is filled in without a Connect Four' do
+			before do
+				col = test_board.slots.select { |slot| slot.coord[1] == 1 }
+				col[0..2].each { |slot| slot.token = "♣" }
+				col[3..5].each { |slot| slot.token = "♥" }
+				test_board.check_columns
+			end
+			it 'does not trigger a victory' do
+				expect(test_board.victory).to be false
+			end
+		end
+	end
+
+	describe '#check_diagonals' do
+		context 'when diagonal slots from top left corner match' do
+			before do
+				diag = test_board.slots.select.with_index { |slot, index| slot if index % 8 == 0 }
+				diag[0..3].each { |slot| slot.token = "♥" }
+			end
+			it 'triggers a victory' do
+				test_board.check_diagonals
+				expect(test_board.victory).to be true
+			end
+		end
+
+		context 'when diagonal slots from top right corner match' do
+			before do
+				diag = [test_board.slots[6]]
+				test_board.slots.each_with_index { |slot, index| diag << slot if index > 6 and (index - 6) % 6 == 0 }
+				diag[0..3].each { |slot| slot.token = "♣" }
+			end
+			it 'triggers a victory' do
+				test_board.check_diagonals
+				expect(test_board.victory).to be true
+			end
+		end
+
+		context 'when diagonal slots in middle of board match' do
+			before do
+				diag = [test_board.slots[8]]
+				test_board.slots.each_with_index { |slot, index| diag << slot if index > 8 and (index - 8) % 8 == 0 }
+				diag[1..4].each { |slot| slot.token = "♣" }
+			end
+			it 'triggers a victory' do
+				test_board.check_diagonals
+				expect(test_board.victory).to be true
+			end
+		end
+
+		context 'when diagonal slots do not match' do
+			before do
+				diag = []
+				test_board.slots.each_with_index { |slot, index| diag << slot if index % 8 == 0 }
+				diag[1..3].each { |slot| slot.token = "♣" }
+				diag[4..5].each { |slot| slot.token = "♥" }
+			end
+			it 'does not trigger a victory' do
+				test_board.check_diagonals
+				expect(test_board.victory).to be false
+			end
+		end
+	end
+end
+
+describe CFPlayer do
+	let(:player1) { CFPlayer.new("♥") }
+	let(:player2) { CFPlayer.new("♣") }
+
+	before(:each) do
+		player1.id = "Player 1"
+		player2.id = "Player 2"
+	end
+
+	after(:each) do
+		player1.board.slots.each { |slot| slot.token = nil }
+		player2.board.slots.each { |slot| slot.token = nil }
+	end
+
+	context 'upon creation' do
+		it 'has a player ID' do
+			expect(player1).to respond_to(:id)
+			expect(player1.id).to_not be nil
+		end
+
+		it 'has an assigned token' do
+			expect(player1).to respond_to(:token)
+			expect(player1.token).to_not be nil
+		end
+	end
+
+	describe '#place_token' do
+		context 'player picks empty column' do
+			before { player1.place_token("3") }
+			it 'places a token at the bottom of the third column' do
+				expect(player1.board.slots[37].token).to eql(player1.token)
+			end
+		end
+
+		context 'player picks column that has tokens' do
+			before do
+				2.times { player1.place_token("5") }
+				player2.place_token("5")
+			end
+			it 'places token at lowest available slot in fifth column' do
+				expect(player2.board.slots[25].token).to eql(player2.token)
+			end
+		end
+	end
+
+	describe '#turn' do
+		after(:each) { $stdin = STDIN }
+		context 'player chooses column 1' do
+			before do 
+				$stdin = StringIO.new("1\n")
+				player1.turn
+			end
+			it 'places token at bottom of column 1' do
+				expect(player1.board.slots[35].token).to eq(player1.token)
+			end
+		end
+
+		context 'player chooses column 5' do
+			before do 
+				$stdin = StringIO.new("5\n")
+				player2.turn
+			end
+			it 'places token at bottom of column 5' do
+				expect(player2.board.slots[39].token).to eq(player2.token)
+			end
+		end
+	end
+
+	describe '#connect_four?' do
+		context 'when a player wins' do
+			before do
+				4.times { player2.place_token("4") }			
+			end
+			output = /.*(Game over! Player 2 wins!)$/
+			it 'returns winning message' do
+				expect{ player2.connect_four? }.to output(output).to_stdout
+			end
+		end
+	end
+
+end
